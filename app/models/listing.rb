8cfbdbdfefdfc2740c7fa809fb1ed08d6dc39eb7
Dayo Adisa <dayoadisa@gmail.com>
2015-02-09 22:53:21 +0200

    Add figaro gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "jp.png"
-	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "jp.png",
+	                 :storage => :dropbox,
+                     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
+
