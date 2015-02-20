0424a576317218af1700e206a35d4bbc27289de7
Dayo Adisa <dayoadisa@gmail.com>
2015-02-20 22:54:52 +0200

    update to the listing.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,14 @@
+
 class Listing < ActiveRecord::Base
-	
-	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "jp.png",
-	                 :storage => :dropbox,
-                     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-    validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  if Rails.env.production?
+    has_attached_file :image,
+    :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "jp.jpg",
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+:path => ":style/:id_:filename"
+    validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+  else
+    has_attached_file :image,
+    :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "djp.jpg"
+  end
 end
-
-
